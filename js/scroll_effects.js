192cc2d629d47c524f6dfbde5ee192ed73a7ddcf
josephmalete1997 <josephmalete75@gmail.com>
2024-10-02 00:03:07 +0200

    New Updates on the portfolio

--- js/scroll_effects.js
+++ js/scroll_effects.js
@@ -3,15 +3,6 @@ const links = document.querySelectorAll(".link");
 const sideNavigation = document.querySelector(".side-nav");
 const logo = document.querySelector(".side-nav h1");
 
-links[0].classList.add("active");
-title[0].style.transform = "scale(1)";
-
-links.forEach((link, i) => {
-  link.addEventListener("click", () => {
-    setActiveLink(i);
-  });
-});
-
 if (window.innerWidth >= 298 && window.innerWidth <= 600) {
   document.querySelectorAll(".link").forEach((link) => {
     link.addEventListener("click", () => {
@@ -23,51 +14,14 @@ if (window.innerWidth >= 298 && window.innerWidth <= 600) {
 let rotateIndexOne = 0;
 let rotateIndexTwo = 0;
 const gears = document.querySelectorAll(".gear-scroll .fa-solid");
-function showAlert() {
-  title.forEach((section, i) => {
-    const rect = section.getBoundingClientRect();
-    const home = document.querySelector("#home").getBoundingClientRect();
-    const about = document.querySelector("#about").getBoundingClientRect();
-    const imageAndText = document.querySelector(".text-image");
-    const hobby = document.querySelector(".panel-one");
-    imageAndText.style.transition = ".5s";
 
-    hobby.style.transition = ".5s";
-
-    if (home.top >= 0) {
-      sideNavigation.style.top = "0px";
-      sideNavigation.style.background = "transparent";
-      sideNavigation.style.backdropFilter = "blur(20px)";
-      // changeLinkColor("white", "black", "white", "white", "transparent");
-      logo.style.color = "white";
-    } else {
-      sideNavigation.style.background = "rgb(255,255,255,0.1)";
-      sideNavigation.style.backdropFilter = "blur(20px)";
-      // changeLinkColor("black", "white", "black", "black", "transparent");
-      sideNavigation.style.top = "0px";
-      logo.style.color = "white";
-    }
-  });
-}
-
-function changeLinkColor(color, over, out, bgOver, bgOut) {
-  links.forEach((item) => {
-    item.style.color = color;
-    item.addEventListener("mouseover", () => {
-      item.style.color = over;
-      item.style.background = bgOver;
-    });
-    item.addEventListener("mouseout", () => {
-      item.style.color = out;
-      item.style.background = bgOut;
-    });
-    item.addEventListener("click", () => {
-      item.style.color = color;
-    });
-  });
-}
-
-window.addEventListener("scroll", showAlert);
+const home = document.querySelector("#home").getBoundingClientRect();
+const nav = document.querySelector(".side-nav").getBoundingClientRect();
+const education = document.querySelector(".Education").getBoundingClientRect();
+const about = document.querySelector("#about").getBoundingClientRect();
+const imageAndText = document.querySelector(".text-image");
+const hobby = document.querySelector(".panel-one");
+imageAndText.style.transition = ".5s";
 
 downloadButton = document.querySelector("#download");
 
